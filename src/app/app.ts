b596fd14f5630ab9785935b3d3068f436a58ca41
Cida Luna <cidalunna@hotmail.com>
2026-04-12 16:41:48 -0300

    feat: add new page empty certificates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
-import { PrimaryButton } from './components/primary-button/primary-button';
-import { SecondaryButton } from './components/secondary-button/secondary-button';
-import { CertificateItem } from './components/certificate-item/certificate-item';
+import { BaseUi } from './components/base-ui/base-ui';
+import { Certificates } from "./pages/certificates/certificates";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CertificateItem],
+  imports: [RouterOutlet, Navbar, BaseUi, Certificates],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ng-gerador-de-certificado');
 
-  showNavBar: boolean = true;
 }
---
+++ src/app/pages/certificates/certificates.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificates',
+  imports: [SecondaryButton],
+  templateUrl: './certificates.html',
+  styleUrl: './certificates.scss',
+})
+export class Certificates {}
