f6fefec0be5aa628cede3b18f961287dccc3c919
Cida Luna <cidalunna@hotmail.com>
2026-04-12 15:56:23 -0300

    feat: start certificate item list component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { PrimaryButton } from './components/primary-button/primary-button';
+import { SecondaryButton } from './components/secondary-button/secondary-button';
+import { CertificateItem } from './components/certificate-item/certificate-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CertificateItem],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
