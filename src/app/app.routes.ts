697c8ab4c95fa5e0fcfd66c93eaa8b0858f87206
Cida Luna <cidalunna@hotmail.com>
2026-06-12 12:23:31 -0300

    feat: update nvm script and some adjust on certificates list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
 import { Certificate } from './pages/certificate/certificate';
+import { Certificates } from './pages/certificates/certificates';
+import { CertificateForm } from './pages/certificate-form/certificate-form';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'certificates',
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path: '',
-  //   component: Certificates
-  // },
   {
     path: '',
+    redirectTo: 'certificados',
+    pathMatch: 'full'
+  },
+  {
+    path: 'certificados',
+    component: Certificates
+  },
+  {
+    path: 'certificados/novo',
+    component: CertificateForm
+  },
+  {
+    path: 'certificado/:id',
     component: Certificate
   }
 ];
