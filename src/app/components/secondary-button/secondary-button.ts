72ea0fff65aab2667ffd072c645a7c8caaef2d19
Cida Luna <cidalunna@hotmail.com>
2026-06-15 08:07:59 -0300

    feat: refactor active link navbar, messages validator error and custom buttons

--- src/app/components/secondary-button/secondary-button.ts
+++ src/app/components/secondary-button/secondary-button.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './secondary-button.html',
   styleUrl: './secondary-button.scss',
 })
-export class SecondaryButton {}
+export class SecondaryButton {
+  @Input({ required: true }) textButton!: string;
+  @Input() iconPhClass!: string;
+  @Input() disabled: boolean = false;
+}
--- src/app/pages/certificates/certificates.ts
+++ src/app/pages/certificates/certificates.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CertificateItem } from "../../components/certificate-item/certificate-item";
+import { SecondaryButton } from '../../components/secondary-button/secondary-button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-certificates',
-  imports: [CertificateItem],
+  imports: [CertificateItem, SecondaryButton, RouterLink],
   templateUrl: './certificates.html',
   styleUrl: './certificates.scss',
 })
